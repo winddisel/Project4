fd56d66f84151eed09003e456b0e0311a79639ab
User <User@DESKTOP-D05IIM6>
2019-05-09 22:59:35 +0300

    phonebook_v1.0

--- Project4/Header.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-struct phone
-{
-	int code_mista;
-	int n_station;
-	int n_obonent;
-};
--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,67 +1,30 @@
 #include <iostream>
 using namespace std;
+class Phone
+{
 
-struct phone
-{
-	int code_mista;
-	int n_station;
-	int n_obonent;
-};
-void print(phone a);
+private:
 
-int k = 0;
-struct employee
-{
-	int tab_nom;
-	float zp;
-	int num_sutr;
+
+public:
+	Phone();
+	~Phone();
+
 };
-employee	insert();
-employee insert()
+
+Phone::Phone()
 {
-	employee sotr;
-	k++;
-	sotr.num_sutr = k;
-	cout << "Please insert tab number of employee->" << endl;
-	cin >> sotr.tab_nom;
-	cout << "Please insert dollar per month of employee->";
-	cin >> sotr.zp;
-	return sotr;
 }
-void print(employee a)
+
+Phone::~Phone()
 {
-	cout << "Employee Number " << a.num_sutr << endl;
-	cout << "Tab number " << a.tab_nom << endl;
-	cout << "Dollar per month " << a.zp << endl;
-	cout << endl;
 }
 
 
-
-void main()
+int main()
 {
-	phone one = { 212,767,8900 };
-	phone You;
-	print(one);
-	cout << "Please insert code mista, number of telephone station, number obonet" << endl;
-	cin >> You.code_mista;
-	cin >> You.n_station;
-	cin >> You.n_obonent;
-	print(You);
-
-	employee	Vasya, Petya, Kolya;
-	Vasya = insert();
-	Petya = insert();
-	Kolya = insert();
-	print(Vasya);
-	print(Petya);
-	print(Kolya);
-
+	Phone abonent1, abonent2;
+	
 	system("pause>>NULL");
-
-}
-
-void print(phone a)
-{
-	cout << "New Number in base-> " << "(" << a.code_mista << ") " << a.n_station << "-" << a.n_obonent << endl;
+	return 0;
 }
