be1f0ac7fc2b61de017ea68eae94ae34110cf9e0
User <User@DESKTOP-D05IIM6>
2019-05-12 11:41:05 +0300

    2

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -4,16 +4,24 @@ class Phone
 {
 
 private:
-
+	const char* FIO[100];
+	char htel;
+	char wtel;
+	char info;
 
 public:
 	Phone();
+	void Show() {
+		cout << FIO[100]<<'\n';
+	}
 	~Phone();
 
 };
 
 Phone::Phone()
 {
+	FIO[100] = "Ivanov Ivan Ivanovich";
+
 }
 
 Phone::~Phone()
@@ -24,7 +32,8 @@ Phone::~Phone()
 int main()
 {
 	Phone abonent1, abonent2;
-	
+	abonent1.Show();
+	abonent2.Show();
 	system("pause>>NULL");
 	return 0;
 }
