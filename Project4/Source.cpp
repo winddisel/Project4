df900dea7bd222fd4b7b9feb494ca7777bb10f98
User <User@DESKTOP-D05IIM6>
2019-05-12 20:42:35 +0300

    5

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,54 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 using namespace std;
-class Phone
+
+class Person
 {
+public:
+	void Print();
+	Person();
+	~Person();
+
+	void write()
+	{
+		cout << "Please insert name->";
+		cin >> name;
+		cout << "Please insert age>";
+		cin >> age;
+		cout << "Please insert 1 for Man, or 0 for woman->";
+		cin >> man;
+		cout << "Please insert phone>";
+		cin >> phone;
+	}
 
 private:
-	char FirstName[20];
-	char LastName[20];
-	int hometel;
-	int worktel;
-	char info[20];
-
-public:
-	Phone();
-	void Show() {
-		cout << LastName << endl;
-		cout << FirstName<<endl;
-		cout << worktel << endl;
-		cout << hometel << endl;
-		cout << info << endl;
-	}
-
-	void Read() {
-		cout << "Please insert First Name - >" << endl;
-		cin >> FirstName;
-		cout << "Please insert Last Name - >" << endl;
-		cin >> LastName;
-	}
-	~Phone();
-
+	char name[10];
+	int age;
+	bool man;
+	char phone[10];
 };
 
-Phone::Phone()
+void Person::Print()
 {
-	FirstName[20] = 'Alex';
-	LastName[20] = 'Usov';
-	hometel = 80987720453;
-	worktel = 80442203070;
-	info[20] = '....';
+	cout << endl;
+	cout << "Name:" << endl;
+	cout << name << endl;
+	cout << "Aage:" << endl;
+	cout << age << endl;
+	cout << "Man-1, or woman-0:" << endl;
+	cout << man << endl;
+	cout << "Phone number:" << endl;
+	cout << phone << endl;
 }
 
-Phone::~Phone()
+Person::Person()
+{
+	strcpy(name, "X");
+	age = 0;
+	man = true;
+	strcpy(phone, "0671234567");
+}
+
+Person::~Person()
 {
 }
 
 
 int main()
 {
-	Phone abonent1, abonent2;
-	abonent2.Read();
-	abonent2.Show();
+	Person p1, p2;
+
+	//редактируем и выводим на консоль
+	p1.write();
+	p1.Print();
+
+	//р2 Создает конструктор
+	//просто выводим на консоль
+	p2.Print();
+
+
+
 	system("pause>>NULL");
 	return 0;
 }
